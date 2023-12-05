19abb7f702149fae505b260f725cdf61d1aebc9e
NunoDesktop <up201807530@fe.up.pt>
2023-12-05 21:50:50 +0000

    Implemented Position

--- src/main/java/pt/up/model/Position.java
+++ src/main/java/pt/up/model/Position.java
@@ -1,4 +1,31 @@
 package pt.up.model;
 
 public class Position {
+    private int x;
+    private int y;
+
+    public Position(int x, int y){
+        this.x= x;
+        this.y= y;
+    }
+
+    public int getX(){
+        return x;
+    }
+    public int getY(){
+        return y;
+    }
+
+    public void modeUp(){
+        y-=1;
+    }
+    public void moveDown(){
+        y+=1;
+    }
+    public void moveRight(){
+        x+=1;
+    }
+    public void moveLeft(){
+        x-=1;
+    }
 }
