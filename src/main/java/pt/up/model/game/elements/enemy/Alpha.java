413281ea2af52a50334e1ded0e689a62cb8a5d67
NunoDesktop <up201807530@fe.up.pt>
2023-12-05 22:42:23 +0000

    ref: Enemies now extend Enemy

--- src/main/java/pt/up/model/game/elements/enemy/Alpha.java
+++ src/main/java/pt/up/model/game/elements/enemy/Alpha.java
@@ -1,4 +1,19 @@
 package pt.up.model.game.elements.enemy;
 
+import pt.up.model.game.elements.Hero;
+
 public class Alpha extends Enemy {
+    public Alpha(int x, int y) {
+        super(x, y);
+    }
+
+    // to check if its working
+    public static void main(String[] args) {
+        Alpha alpha = new Alpha(0,5);
+
+        alpha.setHealth(10);
+        alpha.setPoints(30);
+        System.out.printf("Alpha Health %s\n", alpha.getHealth());
+        System.out.printf("Alpha Points %s\n", alpha.getPoints());
+    }
 }
--- src/main/java/pt/up/model/game/elements/enemy/Gamma.java
+++ src/main/java/pt/up/model/game/elements/enemy/Gamma.java
@@ -1,4 +1,8 @@
 package pt.up.model.game.elements.enemy;
 
 public class Gamma extends Enemy {
+    public Gamma(int x, int y) {
+        super(x, y);
+    }
+
 }
