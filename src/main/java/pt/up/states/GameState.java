15db15527e6bcce0f4916d154c268396ca856579
Artur Telo <up202104487@fe.up.pt>
2023-12-12 22:30:22 +0000

    Update states

--- src/main/java/pt/up/states/GameState.java
+++ src/main/java/pt/up/states/GameState.java
@@ -1,45 +1,74 @@
 package pt.up.states;
 
 import pt.up.controller.Observer;
+import pt.up.controller.PlayerController;
+import pt.up.gui.LanGui;
 import pt.up.model.Model;
+import pt.up.model.game.elements.GameModel;
+import pt.up.utils.Configuration;
+import pt.up.utils.Constants;
+import pt.up.utils.Music;
 import pt.up.viewer.Viewer;
+import pt.up.viewer.game.GameViewer;
 
 import java.io.IOException;
 
 public class GameState extends State {
+    private GameViewer gameViewer;
+    private GameModel gameModel;
+    private PlayerController playerController;
+    LanGui gui;
+    Music music;
+
+    public void initializing() throws IOException {
+        gameModel = new GameModel("map.txt");
+        playerController = new PlayerController(gameModel.getMap().getHero(), gameModel);
+        gui = new LanGui();
+        Configuration.getInstance().stopAllMusic();
+        music = Configuration.getInstance().getGameMusic();
+    }
+
+    public GameState() throws IOException {
+        super();
+        initializing();
+    }
+
+    @Override
+    public void initScreen() {
+        gui.createScreenGame(Constants.GAME_SCREEN_WIDTH, Constants.GAME_SCREEN_HEIGHT);
+        gameViewer = new GameViewer(gameModel, gui.getScreen());
+    }
 
     @Override
     public Viewer getViewer() {
-        return null;
+        return gameViewer;
     }
 
     @Override
     public Observer getObserver() {
-        return null;
+        return playerController;
     }
 
     @Override
     public Model getModel() {
-        return null;
+        return gameModel;
     }
 
     @Override
     public boolean isRunning() {
-        return false;
+        return true;
     }
 
     @Override
     public void step() throws IOException {
-
+        if (!music.isPlaying()) {
+            music.start();
+        }
+        gameViewer.draw();
     }
 
     @Override
     public State nextState() throws IOException {
         return null;
-    }
-
-    @Override
-    public void initScreen() {
-
     }
 }
