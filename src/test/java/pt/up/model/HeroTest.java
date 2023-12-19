3da35a46d93751035ba93103cb01d6ecbdf3cbc5
n00n <up201807530@fe.up.pt>
2023-12-19 13:04:33 +0000

    src: Created a simple and unique test

--- src/test/java/pt/up/model/HeroTest.java
+++ src/test/java/pt/up/model/HeroTest.java
@@ -1,4 +1,22 @@
 package pt.up.model;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import pt.up.model.game.elements.Hero;
 
 public class HeroTest {
+    private Hero hero;
+
+    @BeforeEach
+    public void createHero(){
+        hero = new Hero(10,10);
+    }
+
+    @Test
+    public void decresa(){
+        hero.reduceHeroHealth(1);
+        int heroHealth = hero.getHeroHealth();
+
+        Assertions.assertEquals(2,heroHealth);
+    }
 }
