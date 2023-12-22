d786e93b977ae61e0659f1cff1f8323c3975b2e1
Nuno França <up201807530@fe.up.pt>
2023-12-22 23:40:41 +0000

    src: Tests for menu

---
+++ src/test/java/pt/up/model/menu/HighScoresTest.java
@@ -0,0 +1,10 @@
+package pt.up.model.menu;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+public class HighScoresTest {
+}
